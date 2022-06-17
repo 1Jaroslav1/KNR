48b53d1ccb0bc2fa7592580cf914e5de682d7689
Yaroslav <garbar.jarek@gmail.com>
2022-06-17 22:33:35 +0200

    add footer

--- js/script.js
+++ js/script.js
@@ -1,15 +1,15 @@
-function addActiveNav(navItemsSelector){
+function addActiveNav(navItemsSelector, activeSelector){
     const nav = document.querySelectorAll(navItemsSelector);
     nav.forEach(nav_item => {
         nav_item.addEventListener('click', (e)=>{
             e.preventDefault()
             if(!e.target.querySelector("i") && !e.target.classList.contains("fas")){
                 nav.forEach(i => {
-                    if(i.classList.contains('active')){
-                        i.classList.remove('active');
+                    if(i.classList.contains(activeSelector)){
+                        i.classList.remove(activeSelector);
                     }
                 });
-                e.target.classList.add('active');
+                e.target.classList.add(activeSelector);
             }
         });
     });
@@ -23,19 +23,19 @@ function clickBars(navSelector, navItemsSelector, barsSelector){
 
     hamburger.addEventListener('click', () => {
         nav.classList.toggle('navbar-mobile');
-        hamburger.classList.toggle('fa-bars');
-        hamburger.classList.toggle('fa-times');
+        hamburger.children[0].classList.toggle('fa-bars');
+        hamburger.children[0].classList.toggle('fa-times');
     });
     menuItem.forEach(item => {
         item.addEventListener('click', (e) => {
             nav.classList.remove('navbar-mobile');
-            hamburger.classList.toggle('fa-bars');
-            hamburger.classList.toggle('fa-times');
+            hamburger.children[0].classList.toggle('fa-bars');
+            hamburger.children[0].classList.toggle('fa-times');
         });
     });
 }
 
 window.addEventListener('DOMContentLoaded', function() {
-    addActiveNav('.navbar-link');
+    addActiveNav('.navbar-link', 'active');
     clickBars('.navbar', '.navbar-link', '.mobile-nav-bars');
 });
